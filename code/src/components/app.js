f18cb17ad8eccc96e928e3ac91ee09e1038da295
kmkkkuk <kmkkuk@gmail.com>
2017-11-27 17:56:09 +0100

    Kaisa's design brief articles added

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -4,9 +4,78 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        Find me in src/app.js!
+  <div className="App">
+      <div className="nav-elements">
+
+          <ul>
+            <li>Home</li>
+            <li>Technology</li>
+            <li>Creativity</li>
+            <li>Entrepreneurship</li>
+            <li>Self</li>
+            <li>Culture</li>
+            <li>Digital design</li>
+            <li>Popular on Medium</li>
+            <li>Politics</li>
+            <li>More</li>
+            <li>Get started</li>
+          </ul>
       </div>
+
+      <div className="headline-news">
+          <div className="headline-text">
+            <h1>Interesting ideas that set your mind in motion.</h1>
+            <h5>Hear directly from the people who know it best. From tech to politics to creativity and more -whatever your interest, we've got you covered.
+            </h5>
+          </div>
+          <div className="headline-image">
+
+          </div>
+      </div>
+
+
+      <div className="news-container">
+
+          <div className="news-left">
+              <div className="news-image image-puff-1">
+              </div>
+              <div className="news-piece">
+                  <h2>The Boy Who Tamed the Sea</h2>
+                  <p>At 5:30 am, on any morning I can handle it, I'll slip out of the dark mouth of my covers and creep through the silver lig...</p>
+              </div>
+          </div>
+
+
+          <div className="news-right">
+              <div className="news-image image-puff-2">
+              </div>
+              <div className="news-piece">
+                <h2>The Boy Who Tamed the Sea</h2>
+                <p>At 5:30 am, on any morning I can handle it, I'll slip out of the dark mouth of my covers and creep through the silver lig...</p>
+              </div>
+          </div>
+
+          <div className="news-left">
+              <div className="news-image image-puff-3">
+              </div>
+              <div className="news-piece">
+                <h2>The Boy Who Tamed the Sea</h2>
+                <p>At 5:30 am, on any morning I can handle it, I'll slip out of the dark mouth of my covers and creep through the silver lig...</p>
+              </div>
+          </div>
+
+          <div className="news-right">
+                <div className="news-image image-puff-4">
+                </div>
+                <div className="news-piece">
+                  <h2>The Boy Who Tamed the Sea</h2>
+                  <p>At 5:30 am, on any morning I can handle it, I'll slip out of the dark mouth of my covers and creep through the silver lig...</p>
+                </div>
+          </div>
+
+
+      </div>
+  </div>
     )
   }
 
